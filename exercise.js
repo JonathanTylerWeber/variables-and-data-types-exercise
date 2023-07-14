cfebe0c2f0cd7470f8065eebc947ef1ee825ca08
JonathanTylerWeber <jonathantweber@gmail.com>
2023-07-15 01:39:03 +0800

    done

---
+++ exercise.js
@@ -0,0 +1,31 @@
+//1
+let name='Jonathan';
+
+let dayOfBirth='16';
+
+
+//2
+let variable='one';
+
+let variable='two';
+
+
+//3
+let colorResponse = prompt("what is your favorite color?");
+console.log("Your favorite color is " + colorResponse);
+
+
+//4
+'he said "I don\'t like cats"';
+
+
+//5
+//null is defined by the user and undefined is a value that has not been set
+
+
+//6
+//NaN is not a number, NaN type is 'number' lol
+
+
+//7
+//undefined
